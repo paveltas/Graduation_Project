b17b99f459d65436ad425dc6c4e488949b2a0cd9
paveltas <124336776+paveltas@users.noreply.github.com>
2023-07-26 19:55:11 +0300

    add authorization screen

--- frontend/armored_train/main.py
+++ frontend/armored_train/main.py
@@ -2,62 +2,61 @@ import pygame
 
 pygame.init()
 
-# Установка разрения экрана
 screen = pygame.display.set_mode((1920, 1080))
 
-# Загрузка фонового изображения
 background_image = pygame.image.load("game/assets/images/background.jpg").convert()
 
-# Загрузка стилизованных шрифтов
 font_title = pygame.font.Font("game/assets/fonts/Oswald-Light.ttf", 72)
 font_label = pygame.font.Font("game/assets/fonts/Oswald-Light.ttf", 36)
 font_input = pygame.font.Font("game/assets/fonts/Oswald-Light.ttf", 36)
 
-# Цвета
 WHITE = (255, 255, 255)
 
-# Переменные для хранения данных пользователя
 username = ""
 password = ""
 active_input_box = None
 
 input_boxes = [
     {
-        'rect': pygame.Rect(800, 400, 400, 50),  # Поле ввода для username
-        'text': '',
-        'active': False  # Флаг активности поля ввода
+        'rect': pygame.Rect(800, 400, 400, 50),
+        'active': False,
+        'text': ''
     },
     {
-        'rect': pygame.Rect(800, 500, 400, 50),  # Поле ввода для password
-        'text': '',
-        'active': False
+        'rect': pygame.Rect(800, 500, 400, 50),
+        'active': False,
+        'text': ''
     }
 ]
+
+auth_button_rect = pygame.Rect(800, 600, 200, 50)
+auth_button_active = False
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Левая кнопка мыши
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            for box in input_boxes:
-                if box['rect'].collidepoint(mouse_pos):
-                    box['active'] = True
-                    active_input_box = box
-                else:
-                    box['active'] = False
-                    active_input_box = None
+            if event.button == 1:
+                for box in input_boxes:
+                    if box['rect'].collidepoint(mouse_pos):
+                        box['active'] = True
+                        active_input_box = box
+                    else:
+                        box['active'] = False
+                        active_input_box = None
+                if auth_button_rect.collidepoint(mouse_pos):
+                    username = input_boxes[0]['text']
+                    password = input_boxes[1]['text']
+                    input_boxes[0]['text'] = ''
+                    input_boxes[1]['text'] = ''
 
         elif event.type == pygame.KEYDOWN:
             if active_input_box:
                 if event.key == pygame.K_RETURN:
-                    # Обработка введенных данных пользователя
-                    if active_input_box['rect'] == input_boxes[0]['rect']:
-                        username = active_input_box['text']
-                    elif active_input_box['rect'] == input_boxes[1]['rect']:
-                        password = active_input_box['text']
-                    active_input_box['text'] = ""  # Сброс текста поля ввода
+                    active_input_box['text'] = ''
                 elif event.key == pygame.K_BACKSPACE:
                     active_input_box['text'] = active_input_box['text'][:-1]
                 elif len(active_input_box['text']) < 20:
@@ -65,26 +64,27 @@ while running:
 
     screen.blit(background_image, (0, 0))
 
-    # Отрисовка заголовка
     title_text = font_title.render("Авторизация", True, WHITE)
     screen.blit(title_text, (750, 100))
 
-    # Отрисовка полей для ввода
     username_label = font_label.render("Username:", True, WHITE)
     password_label = font_label.render("Password:", True, WHITE)
     screen.blit(username_label, (600, 400))
     screen.blit(password_label, (600, 500))
 
-    # Отрисовка рамок вокруг активных полей ввода
     for box in input_boxes:
         color = (255, 0, 0) if box['active'] else (0, 0, 0)
         pygame.draw.rect(screen, color, box['rect'], 2)
 
-    # Отрисовка введенного текста
     username_input = font_input.render(input_boxes[0]['text'], True, WHITE)
     password_input = font_input.render("*" * len(input_boxes[1]['text']), True, WHITE)
     screen.blit(username_input, (800, 400))
     screen.blit(password_input, (800, 500))
+
+    auth_button_color = (255, 0, 0) if auth_button_active else (0, 0, 0)
+    pygame.draw.rect(screen, auth_button_color, auth_button_rect, 2)
+    auth_button_text = font_label.render("Авторизация", True, WHITE)
+    screen.blit(auth_button_text, (810, 610))
 
     pygame.display.flip()
 
